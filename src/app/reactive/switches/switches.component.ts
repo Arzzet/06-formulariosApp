e1c7e5b6c590f4c05de483bd57b0295d608b46eb
Iwo <iwoskater3@hotmail.com>
2022-02-17 11:58:51 +0100

    reactive forms for switches

--- src/app/reactive/switches/switches.component.ts
+++ src/app/reactive/switches/switches.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-switches',
@@ -6,11 +8,43 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class SwitchesComponent implements OnInit {
+export class SwitchesComponent implements OnInit{
 
-  constructor() { }
+  miFormulario: FormGroup = this.fb.group({
+    genero: ['M', Validators.required],
+    notificaciones: [true, Validators.required],
+    condiciones: [false, Validators.requiredTrue]
+  });
+
+  friden = {
+    genero: 'F',
+    notificaciones: true,
+  }
+
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
+    this.miFormulario.reset({
+      ...this.friden,
+      condiciones: false
+    });
+
+    this.miFormulario.valueChanges.subscribe(
+      ({condiciones, ...rest}) => {
+        this.friden = rest;
+    });
+
+    // this.miFormulario.get('condiciones')?.valueChanges.subscribe(
+    //   newValue => console.log(newValue));
+  }
+  
+
+  guardar() {
+    const formValue = {...this.miFormulario.value};
+    delete formValue.condiciones
+
+    this.friden = formValue;
+    console.log(formValue)
   }
 
 }
