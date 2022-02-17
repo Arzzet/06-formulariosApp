cac633b3ae42467e24fd9a9eebe0b84197446c5f
Iwo <iwoskater3@hotmail.com>
2022-02-17 11:25:40 +0100

    Reactive forms approach or dynamic forms.

--- src/app/reactive/basicos/basicos.component.ts
+++ src/app/reactive/basicos/basicos.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-basicos',
@@ -7,12 +7,43 @@ import { FormControl, FormGroup } from '@angular/forms';
   styles: [
   ]
 })
-export class BasicosComponent {
+export class BasicosComponent implements OnInit{
 
-  miFormulario: FormGroup = new FormGroup({
-    nombre: new FormControl('RTX4090Ti')
+  // miFormulario: FormGroup = new FormGroup({
+  //   nombre: new FormControl('RTX4090Ti'),
+  //   precio: new FormControl(1200),
+  //   existencias: new FormControl('5')
+  // });
+
+  miFormulario: FormGroup = this.fb.group({
+    nombre: [ , [Validators.required, Validators.minLength(3)]],
+    precio: [ , [Validators.min(0), Validators.required]],
+    existencias: [ ,[Validators.min(0), Validators.required]]
   });
 
-  constructor() { }
+  constructor(private fb: FormBuilder) { }
+
+  ngOnInit(): void {
+    // this.miFormulario.reset({
+    //   nombre: 'RTX4090Ti',
+    //   precio: 1200
+    //   // existencias: 5
+    // });
+  }
+
+  campoNotValid(){
+    return this.miFormulario.controls.nombre.errors && this.miFormulario.controls.nombre.touched
+  }
+
+  guardar(){
+    if (this.miFormulario.invalid){
+      this.miFormulario.markAllAsTouched();
+      return
+    }
+    else{
+      console.log(this.miFormulario.value);
+      this.miFormulario.reset();
+    }
+  }
 
 }
